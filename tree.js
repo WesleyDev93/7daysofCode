2b62611d171ed23fc8792a759bd14100eb24603f
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-07-10 18:57:46 -0300

    fazendo o e salvando tree.js

---
+++ tree.js
@@ -0,0 +1,29 @@
+let frontend 
+let backend  
+let resposta1 
+let resposta2 
+
+resposta1 = "frontend";
+resposta2 = "backend";
+
+
+frontend = resposta1;
+backend = resposta2;
+
+
+prompt ('Quer seguir para a area de front end ou back-end ?');
+
+ 
+
+ 
+ 
+ if (frontend == resposta1){
+    prompt ('voce quer react, ou Vue ?');
+ } 
+ if (backend == resposta2) {
+    prompt ('voce quer java, ou c# ?');
+ }
+
+    
+
+
