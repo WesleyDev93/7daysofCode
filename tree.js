6cc362cfba81c9a731e7692135a6ad4885ddc280
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-07-11 11:02:32 -0300

    alteraçoes conforme correção tree

--- tree.js
+++ tree.js
@@ -1,29 +1,50 @@
-let frontend 
-let backend  
-let resposta1 
-let resposta2 
+// day tree - analise do codigo 
 
-resposta1 = "frontend";
-resposta2 = "backend";
+const area = prompt ("voce quer seguir para a area de 'frontend' ou de 'backend'?");
+let linguagem = "";
+
+if (area === "frontend"){
+   linguagem = prompt ("voce quer aprender react ou vue?");
+
+}
+else if (area === "backend");{
+linguagem = prompt ("voce quer aprender C# ou java?");
+} 
+
+else {
+   alert("voce nao inseriu uma area valida!");
+}
 
 
-frontend = resposta1;
-backend = resposta2;
+const especialidadeoufullstack = prompt("digite 1 para seguir se especializando na area escolhida ou 2 para continuar se desenvolvendo para se tornar full stack");
+if (especialidadeoufullstack ==1);{
+      alert (`continue a se especializar em ${linguagem} para dominar a area de ${area}!`);
+
+}
 
 
-prompt ('Quer seguir para a area de front end ou back-end ?');
+
+else if (especialidadeoufullstack == 2){
+   alert (`chegou a hora de começar a aprender outras linguagens alem de ${linguagem} se voce quer se tornar fullstack`);
+}
+else {
+   alert (" voce nao inseriu uma opção valida!");
+
+}
+
+let msg = prompt ("tem mais alguma tecnologia que voce gostaria de aprender ? digite 'ok' em caso postivo");
+while (msg === 'ok'){
+   let novatecnologia = prompt ("qual ?");
+   alert (`${novatecnologia} é realmente muito boa !`);
+   msg = prompt ("tem mais alguma tecnologia que voce gostaria de aprender ? digite 'ok' em caso postivo");
+}
+
+
 
  
 
  
  
- if (frontend == resposta1){
-    prompt ('voce quer react, ou Vue ?');
- } 
- if (backend == resposta2) {
-    prompt ('voce quer java, ou c# ?');
- }
-
     
 
 
